0dac05afc00aadc989b5112e017a8acd911eb703
Peter Kaizer <pdkaizer@hey.com>
2026-07-30 20:18:57 -0400

    Add Platformer to podcasts list

--- src/_data/podcasts.js
+++ src/_data/podcasts.js
@@ -8,6 +8,7 @@ const rawPodcasts = [
   { url: "https://podcasts.apple.com/us/podcast/hard-fork/id1528594034" },
   { url: "https://podcasts.apple.com/us/podcast/decoder-with-nilay-patel/id1011668648" },
   { url: "https://podcasts.apple.com/us/podcast/rework/id1264193508" },
+  { url: "https://podcasts.apple.com/us/podcast/platformer/id1868844067" },
 ];
 
 function getMetaTag(html, property) {
