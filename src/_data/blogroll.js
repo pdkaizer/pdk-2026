0c9d123b614841c032dc995600787358f7aa342f
Peter Kaizer <pdkaizer@hey.com>
2026-08-02 15:45:02 -0400

    Alphabetize the blogroll

--- src/_data/blogroll.js
+++ src/_data/blogroll.js
@@ -1,16 +1,16 @@
 // Add new blogs here — name, url, and a short note on why it's worth following.
 const blogroll = [
-  { name: "Jeffrey Zeldman Presents", url: "https://zeldman.com", note: "Jeffrey Zeldman on web design, digital culture, and typography." },
   { name: "A Whole Lotta Nothing", url: "https://a.wholelottanothing.org", note: "Matt Haughey on woodworking, home automation, EVs, and technology." },
+  { name: "Brilliantcrank", url: "https://brilliantcrank.com/", note: "Essays and zines on creative thinking, technology, and culture." },
   { name: "Daring Fireball", url: "https://daringfireball.net", note: "John Gruber on Apple, technology, design, and internet culture." },
+  { name: "Elliott Richmond", url: "https://elliottrichmond.co.uk", note: "Elliott Richmond on WordPress and WooCommerce development." },
+  { name: "Jeffrey Zeldman Presents", url: "https://zeldman.com", note: "Jeffrey Zeldman on web design, digital culture, and typography." },
+  { name: "Josh W Comeau", url: "https://www.joshwcomeau.com/", note: "Josh W Comeau's tutorials and interactive guides on CSS, animation, and React." },
+  { name: "Maggie Appleton's Digital Garden", url: "https://maggieappleton.com/", note: "Maggie Appleton's visual essays on programming, design, and anthropology." },
+  { name: "Paul Stamatiou", url: "https://paulstamatiou.com", note: "Paul Stamatiou's corner of the internet. Lots of interesting stuff." },
   { name: "Stuff & Nonsense", url: "https://stuffandnonsense.co.uk", note: "Andy Clarke's independent design studio, writing on web design and craft." },
   { name: "Weightshifting", url: "https://nazhamid.com", note: "Naz Hamid's occasional notes on design, culture, and travel." },
-  { name: "Paul Stamatiou", url: "https://paulstamatiou.com", note: "Paul Stamatiou's corner of the internet. Lots of interesting stuff." },
-  { name: "Elliott Richmond", url: "https://elliottrichmond.co.uk", note: "Elliott Richmond on WordPress and WooCommerce development." },
   { name: "Zell Liew", url: "https://zellwk.com/", note: "Zell Liew on web development, product building, and personal growth." },
-  { name: "Brilliantcrank", url: "https://brilliantcrank.com/", note: "Essays and zines on creative thinking, technology, and culture." },
-  { name: "Maggie Appleton's Digital Garden", url: "https://maggieappleton.com/", note: "Maggie Appleton's visual essays on programming, design, and anthropology." },
-  { name: "Josh W Comeau", url: "https://www.joshwcomeau.com/", note: "Josh W Comeau's tutorials and interactive guides on CSS, animation, and React." },
 ];
 
 module.exports = blogroll;
